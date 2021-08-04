fee43431195641cb5be78a7da8051f8d28fd2a87
Pablo <dev@digitallab.agency>
2021-08-04 12:26:16 -0600

    added js file for calculate operations

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,8 @@
-function CalculatePrice() {  
-   var x,y,suma,text;  
-   x = document.getElementById("num1").value;  
-   y = document.getElementById("num2").value;  
-   if (isNaN(x) || isNaN(y)) {  
-     text = "Es necesarios introducir dos números válidos";  
-   } else {  
-     //si no ponemos parseFloat concatenaría x con y  
-     suma=parseFloat(x)+parseFloat(y);  
-     text= suma;  
-   }  
-   document.getElementById("sumando").innerHTML = text;  
-}
+
+jQuery('.sub-paquete .basic').click(function() {
+
+  console.log(jQuery(this).data('price'));
+
+});
+
+jQuery('.sub-paquete .basic').data('price');
