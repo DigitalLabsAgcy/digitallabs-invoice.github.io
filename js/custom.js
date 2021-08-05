f9498af77bc9db52036418405fb33107fdc11a90
Pablo <dev@digitallab.agency>
2021-08-05 17:47:13 -0600

    adding style for button cotizar and functionality

--- js/custom.js
+++ js/custom.js
@@ -4,10 +4,11 @@ jQuery(document).ready(function() {
   var contenido = "";
   var diseño = "";
   var pagContenido = "";
+  var dev = "";
   var total = 0;
 
   jQuery('.content-support .widget').click(function() {
-    jQuery('.widget').removeClass('active');
+    jQuery('.content-support .widget').removeClass('active');
     jQuery(this).addClass('active');
     contenido = jQuery(this).data('price');
 
@@ -15,7 +16,7 @@ jQuery(document).ready(function() {
   });
 
   jQuery('.design-support .widget').click(function() {
-    jQuery('.widget').removeClass('active');
+    jQuery('.design-suppor .widget').removeClass('active');
     jQuery(this).addClass('active');
     diseño = jQuery(this).data('price');
 
@@ -27,8 +28,24 @@ jQuery(document).ready(function() {
     
   }
 
+  jQuery('.so-dev .widget').click(function() {
+    jQuery('.so-dev .widget').removeClass('active');
+    jQuery(this).addClass('active');
+    dev = jQuery(this).data('price');
+
+    console.log( "precio: " + dev);
+  });
+
+  jQuery('.membresias-section .price-widget').click(function() {
+    jQuery('.membresias-section .price-widget').removeClass('active');
+    jQuery(this).addClass('active');
+    member = jQuery(this).data('price');
+
+    console.log( "precio: " + member);
+  });
+
   jQuery('.membresias-section .cotizar').click(function() {
-    total = (parseInt(contenido) + parseInt(diseño));
+    total = (parseInt(contenido) + parseInt(diseño) + parseInt(dev) + parseInt(member));
 
     console.log("Total:" + total);
   });
